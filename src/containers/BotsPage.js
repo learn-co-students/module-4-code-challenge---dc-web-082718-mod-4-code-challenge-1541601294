2879926f2870d82c4bbab5850bb1b05c0c591c33
patti-c <patricia.carter@flatironschool.com>
2018-11-07 10:10:33 -0500

    Checkpoint

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,82 @@
 import React from "react";
+import Adapter from '../Adapter'
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const api = new Adapter()
+const BOTSURL = 'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  constructor() {
+    super()
+    this.state = {
+      bots: [],
+      army: []
+    }
+  }
+
+  componentDidMount() {
+    api.genericFetch(BOTSURL).then(this.setBotsState)
+  }
+
+  setBotsState = (data) => {
+    this.setState({
+      bots: data
+    })
+  }
+
+  addToArmy = (bot) => {
+    const botsCopy = this.state.bots
+    botsCopy.map(function(botCopy) {
+      if(botCopy === bot) {
+        botCopy.enlisted = true
+      }
+    })
+
+    this.setState({
+      bots: botsCopy
+    })
+
+  }
+
+  removeFromArmy = (bot) => {
+    const botsCopy = this.state.bots
+    botsCopy.map(function(botCopy) {
+      if(botCopy === bot) {
+        botCopy.enlisted = false
+      }
+    })
+
+    this.setState({
+      bots: botsCopy
+    })
+  }
+
+  // updateBot = (bot, key, value) => {
+  //   const botsCopy = this.state.bots
+  //   botsCopy.map(function(botCopy) {
+  //     if(botCopy === bot) {
+  //       botCopy[key] = value
+  //     }
+  //   })
+  //
+  //   this.setState({
+  //     bots: botsCopy
+  //   })
+  // }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy
+          bots={this.state.bots.filter(bot => bot.enlisted === true)}
+          handleClick={this.removeFromArmy}
+        />
+        <BotCollection
+          bots={this.state.bots.filter(bot => bot.enlisted !== true)}
+          handleClick={this.addToArmy}
+        />
       </div>
     );
   }
